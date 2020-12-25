effe7e9690e8b518baefe794e9a7f5daa358c4f8
djkim1991 <djkim1991@naver.com>
2020-12-25 19:00:01 +0900

    add [프로젝트] 프로젝트 생성

---
+++ src/main/java/com/danbi/danbiquiz/DanbiquizApplication.java
@@ -0,0 +1,13 @@
+package com.danbi.danbiquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanbiquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanbiquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danbi/danbiquiz/DanbiquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danbi.danbiquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanbiquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
